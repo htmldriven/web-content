94a559ba79adcc5ce1ff0dbcaaaa7ef52bb68758
RebendaJiri <jiri.rebenda@htmldriven.com>
2016-02-20 11:18:29 +0100

    index.html: meta attributes updated, sitemap.xml & robots.txt added

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+(function($, window, document, undefined) {
+	$(document).ready(function() {
+		$('.level-bar-inner').css('width', '0');
+
+		$(window).on('load', function() {
+			$('.level-bar-inner').each(function() {
+				var itemWidth = $(this).data('level');
+
+				$(this).animate({
+					width: itemWidth
+				}, 800);
+			});
+		});
+		
+		$('.level-label').tooltip();
+
+		GitHubCalendar("#github-graph", "rebendajirijr");
+		
+		GitHubActivity.feed({ username: "rebendajirijr", selector: "#ghfeed" });
+	});
+})(jQuery, window, document);
